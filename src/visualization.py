7ea7c5ad29e05b3e1ef2e40dac99662c5c95808a
Taeksoo-Kim <51980269+Taeksoo-Kim@users.noreply.github.com>
2021-07-18 18:49:52 +0900

    Update visualization.py
    added function for visualizing batch data

--- src/visualization.py
+++ src/visualization.py
@@ -47,8 +47,50 @@ def visualize_dataset(dataset, idx):
         ax2.set_title("mask")
         
         plt.suptitle("<{} - {}>".format(dataset.category, sample['label']), y=0.9)
+        
 
 for idx in range(3):
     visualize_dataset(train, idx)
     visualize_dataset(validate, idx)
     visualize_dataset(test, idx)
+        
+        
+def visualize_batch(sample_batched):
+"""
+For a batch of samples, show image with its corresponding mask.
+If an image doesn't have a mask, the area for the mask is empty.
+"""
+batch_size = len(sample_batched['image'])
+fig = plt.figure()
+
+for i in range(batch_size):
+    ax1 = fig.add_subplot(2, batch_size, i + 1)
+    plt.imshow(sample_batched['image'][i])
+    plt.axis('off')
+    ax1.set_title("image")
+
+    ax2 = fig.add_subplot(2, batch_size, batch_size + i + 1)
+    plt.imshow(sample_batched['mask'][i], cmap='gray')
+    plt.axis('off')
+    ax2.set_title("mask")
+
+
+train_dataloader = DataLoader(train, batch_size=4, shuffle=True, num_workers=0)
+validate_dataloader = DataLoader(validate, batch_size=4, shuffle=True, num_workers=0)
+test_dataloader = DataLoader(test, batch_size=4, shuffle=True, num_workers=0)
+
+
+for i_batch, sample_batched in enumerate(train_dataloader):
+    if i_batch == 0:
+        visualize_batch(sample_batched)
+        break
+        
+for i_batch, sample_batched in enumerate(validate_dataloader):
+    if i_batch == 0:
+        visualize_batch(sample_batched)
+        break
+
+for i_batch, sample_batched in enumerate(test_dataloader):
+    if i_batch == 0:
+        visualize_batch(sample_batched)
+        break
